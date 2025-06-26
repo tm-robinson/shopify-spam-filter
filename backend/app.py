afff2b6de1a301b01333e4b22f64a9aab1e828ca
thomas.robinson <thomas.robinson@gmail.com>
2025-06-26 06:05:09 +0000

    fix scopes

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,7 @@ CLIENT_CONFIG = {
     }
 }
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
+SCOPES = ["https://www.googleapis.com/auth/gmail.modify", "https://www.googleapis.com/auth/gmail.settings.basic"]
 
 
 @app.route("/")
